d192d4b3e1a0cd838efa6a5a3c8ee48971d5bfb4
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-12 21:42:06 +0530

    add: logo component

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+const font = Poppins({
+  subsets: ["latin-ext"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <div className="bg-white rounded-full p-1">
+        <Image src="/logo.svg" alt="logo" height="80" width="80" />
+      </div>
+      <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold">Trinity</p>
+      </div>
+    </div>
+  );
+};

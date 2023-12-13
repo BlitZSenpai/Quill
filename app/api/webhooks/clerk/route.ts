b03d1ff8003f275c7655f0964d222ff9088c1a0c
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-13 18:43:09 +0530

    changes

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,11 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error("Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local");
+  }
+}

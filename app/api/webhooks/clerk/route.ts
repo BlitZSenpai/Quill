a1bced4176afaa9db1bcbb65dc1a30c98a76cfab
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-14 21:34:30 +0530

    add: update func

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -54,14 +54,6 @@ export async function POST(req: Request) {
   }
 
   if (eventType === "user.updated") {
-    const currentUser = await db.user.findUnique({
-      where: {
-        externalUserId: payload.data.id,
-      },
-    });
-
-    if (!currentUser) return new Response("user not found");
-
     await db.user.update({
       where: {
         externalUserId: payload.data.id,
@@ -69,6 +61,14 @@ export async function POST(req: Request) {
       data: {
         username: payload.data.username,
         imageUrl: payload.data.image_url,
+      },
+    });
+  }
+
+  if (eventType === "user.deleted") {
+    await db.user.delete({
+      where: {
+        externalUserId: payload.data.id,
       },
     });
   }

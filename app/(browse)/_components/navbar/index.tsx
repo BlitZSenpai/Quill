6ab2a9ce8651dae4d67bb29218cf0967f528f022
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-14 23:24:53 +0530

    add: search bar component and query string package

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,9 +1,11 @@
 import { Logo } from "./logo";
+import { Search } from "./search";
 
 export const Navbar = () => {
   return (
     <nav className="fixed top-0 z-[49] w-full h-20 px-2 lg:px-4 flex items-center justify-between shadow-sm bg-[#252731]">
       <Logo />
+      <Search />
     </nav>
   );
 };

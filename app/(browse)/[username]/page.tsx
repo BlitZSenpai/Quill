8474c81b5d3a8816fd769050a0eeed006978f132
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-17 23:20:03 +0530

    add: user service

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,5 +1,20 @@
-const UserPage = () => {
-  return <div>User Page</div>;
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+
+interface UserPageProps {
+  params: {
+    username: string;
+  };
+}
+
+const UserPage = async ({ params }: UserPageProps) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user) {
+    notFound();
+  }
+
+  return <div>User Page{user?.username}</div>;
 };
 
 export default UserPage;

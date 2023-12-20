baaeda2d53b7093b50a56aea12f6463af85e34f4
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-20 20:20:07 +0530

    add: block actions

---
+++ actions/block.ts
@@ -0,0 +1,34 @@
+import { blockUser, unBlockUser } from "@/lib/block-service";
+import { revalidatePath } from "next/cache";
+
+export const onBlock = async (id: string) => {
+  try {
+    const blockedUser = await blockUser(id);
+
+    revalidatePath("/");
+
+    if (blockedUser) {
+      revalidatePath(`/${blockedUser.blocked.username}`);
+    }
+
+    return blockedUser;
+  } catch (error) {
+    throw new Error("Internal Error");
+  }
+};
+
+export const onUnBlock = async (id: string) => {
+  try {
+    const unBlockedUser = await unBlockUser(id);
+
+    revalidatePath("/");
+
+    if (unBlockedUser) {
+      revalidatePath(`/${unBlockedUser.blocked.username}`);
+    }
+
+    return unBlockedUser;
+  } catch (error) {
+    throw new Error("Internal Error");
+  }
+};

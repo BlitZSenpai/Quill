a175ecf4a014d1117882a1ef1f66888c30a43a31
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-23 18:47:53 +0530

    fix: sidebar flicker fix

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -27,8 +27,8 @@ export const Wrapper = ({ children }: WrapperProps) => {
   return (
     <aside
       className={cn(
-        "fixed left-0 w-60 h-full flex flex-col bg-background border-r border-[#2D2E35] z-50",
-        collapsed && "w-[70px]"
+        "fixed left-0 w-[70px] lg:w-60 h-full flex flex-col bg-background border-r border-[#2D2E35] z-50",
+        collapsed && "lg:w-[70px]"
       )}>
       {children}
     </aside>

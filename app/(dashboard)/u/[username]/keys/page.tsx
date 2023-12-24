8d9c79d56cbbdb2bec424d8cd0ca47cf00928ff0
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-24 22:25:53 +0530

    add: shadcn components and connection modal ui

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { UrlCard } from "./_components/url-card";
 import { getUser } from "@/lib/auth-service";
 import { getStreamByUserId } from "@/lib/stream-service";
+import { ConnectModal } from "./_components/connect-modal";
 import { KeyCard } from "./_components/key-card";
+import { UrlCard } from "./_components/url-card";
 
 const KeysPage = async () => {
   const currentUser = await getUser();
@@ -12,7 +12,7 @@ const KeysPage = async () => {
     <div className="p-6">
       <div className="flex items-center justify-between mb-4">
         <h1 className="text-2xl font-bold">Keys & URLs</h1>
-        <Button variant="primary">Generate</Button>
+        <ConnectModal />
       </div>
       <div className="space-y-4">
         <UrlCard value={stream.serverUrl} />

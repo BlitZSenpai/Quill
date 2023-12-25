2f8a5c53756ced166c8bbe11cf45bf382a7a2df1
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-25 22:38:51 +0530

    add: security optimizations

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -128,7 +128,11 @@ export const getFollowedUsers = async () => {
       include: {
         following: {
           include: {
-            stream: true,
+            stream: {
+              select: {
+                isLive: true,
+              },
+            },
           },
         },
       },

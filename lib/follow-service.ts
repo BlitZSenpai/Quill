a73d8f06f381ad7f6029d0c69a7a6a1e301ab180
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-25 21:37:14 +0530

    update: follow updates

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -126,7 +126,11 @@ export const getFollowedUsers = async () => {
         },
       },
       include: {
-        following: true,
+        following: {
+          include: {
+            stream: true,
+          },
+        },
       },
     });
 

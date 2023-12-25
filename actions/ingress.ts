9c680bfc8b8ac4c8f13de93b91af656acc53d558
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-25 20:13:30 +0530

    add: create ingree and livekit packages

---
+++ actions/ingress.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import { getUser } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+import {
+  IngressAudioEncodingPreset,
+  IngressInput,
+  IngressClient,
+  IngressVideoEncodingPreset,
+  RoomServiceClient,
+  type CreateIngressOptions,
+} from "livekit-server-sdk";
+
+import { TrackSource } from "livekit-server-sdk/dist/proto/livekit_models";
+import { revalidatePath } from "next/cache";
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
+
+const ingressClient = new IngressClient(process.env.LIVEKIT_API_URL!);
+
+export const createIngress = async (ingressType: IngressInput) => {
+  const user = await getUser();
+
+  const options: CreateIngressOptions = {
+    name: user.username,
+    roomName: user.id,
+    participantName: user.username,
+    participantIdentity: user.id,
+  };
+
+  if (ingressType === IngressInput.WHIP_INPUT) {
+    options.bypassTranscoding = true;
+  } else {
+    options.video = {
+      source: TrackSource.CAMERA,
+      preset: IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS,
+    };
+    options.audio = {
+      source: TrackSource.MICROPHONE,
+      preset: IngressAudioEncodingPreset.OPUS_STEREO_96KBPS,
+    };
+  }
+
+  const ingress = await ingressClient.createIngress(ingressType, options);
+
+  if (!ingress || !ingress.url || !ingress.streamKey) {
+    throw new Error("Failed to create ingress");
+  }
+
+  await db.stream.update({
+    where: { id: user.id },
+    data: {
+      ingressId: ingress.ingressId,
+      serverUrl: ingress.url,
+      streamKey: ingress.streamKey,
+    },
+  });
+
+  revalidatePath(`/u/${user.username}/keys`);
+
+  return ingress;
+};

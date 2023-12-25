8abc13a24fbd6f273eab3e9f4a9309b74cdd73c0
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-25 23:30:16 +0530

    add: dashboard page

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,5 +1,25 @@
-const DashboardPage = () => {
-  return <div>dashboard DashboardPage</div>;
+import { StreamPlayer } from "@/components/stream-player";
+import { getUserByUsername } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs";
+
+interface DashboardPageProps {
+  params: {
+    username: string;
+  };
+}
+
+const DashboardPage = async ({ params }: DashboardPageProps) => {
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(params.username);
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+  return (
+    <div className="h-full">
+      <StreamPlayer user={user} stream={user.stream} isFollowing />
+    </div>
+  );
 };
 
 export default DashboardPage;

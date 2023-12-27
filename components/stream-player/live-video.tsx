df522c45696875a2058254957cf4e29c376a5918
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-12-27 22:34:34 +0530

    add: live video p-1

---
+++ components/stream-player/live-video.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Participant } from "livekit-client";
+import { useRef } from "react";
+
+interface LiveVideoProps {
+  participant: Participant;
+}
+
+export const LiveVideo = ({ participant }: LiveVideoProps) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  return (
+    <div ref={wrapperRef} className="relative h-full flex">
+      <video ref={videoRef} width="100%" />
+    </div>
+  );
+};

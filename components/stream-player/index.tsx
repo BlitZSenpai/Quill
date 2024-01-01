05fa4c06085ff654efdc57ec3ee5822c2523e843
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-01 22:34:19 +0530

    add: loading page and animation

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -5,8 +5,8 @@ import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { LiveKitRoom } from "@livekit/components-react";
 import { Stream, User } from "@prisma/client";
-import { Chat } from "./chat";
-import { Video } from "./video";
+import { Chat, ChatSkeleton } from "./chat";
+import { Video, VideoSkeleton } from "./video";
 import { ChatToggle } from "./chat-toggle";
 
 interface StreamPlayerProps {
@@ -20,7 +20,7 @@ export const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) =
   const { token, name, identity } = useViewerToken(user.id);
 
   if (!token || !name || !identity) {
-    return <div>Cannot watch the stream</div>;
+    return <StreamPlayerSkeleton />;
   }
 
   return (
@@ -53,5 +53,18 @@ export const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) =
         </div>
       </LiveKitRoom>
     </>
+  );
+};
+
+export const StreamPlayerSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        <VideoSkeleton />
+      </div>
+      <div className="col-span-1 bg-background">
+        <ChatSkeleton />
+      </div>
+    </div>
   );
 };

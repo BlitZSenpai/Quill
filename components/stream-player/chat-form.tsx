18d800352b94bb8bdbd08e88ca3aa6097a391a40
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-01 12:40:16 +0530

    first commit of 2024

--- components/stream-player/chat-form.tsx
+++ components/stream-player/chat-form.tsx
@@ -3,6 +3,8 @@
 import { cn } from "@/lib/utils";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useState } from "react";
+import { Skeleton } from "../ui/skeleton";
 
 interface ChatFormProps {
   onSubmit: () => void;
@@ -23,22 +25,58 @@ export const ChatForm = ({
   isDelayed,
   isFollowing,
 }: ChatFormProps) => {
+  const [isDelayedBlock, setIsDelayedBlock] = useState(false);
+
+  const isFollowersOnlyAndNotFollowing = isFollowersOnly && !isFollowing;
+  const isDisabled = isHidden || isFollowersOnlyAndNotFollowing || isDelayedBlock;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!value && isDisabled) return;
+
+    if (isDelayed && !isDelayedBlock) {
+      setIsDelayedBlock(true);
+      setTimeout(() => {
+        setIsDelayedBlock(false);
+        onSubmit();
+      }, 3000);
+    } else {
+      onSubmit();
+    }
+  };
+
+  if (isHidden) return null;
+
   return (
-    <form onSubmit={() => {}} className="flex flex-col gap-y-4 p-3 items-center">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-y-4 p-3 items-center">
       <div className="w-full">
         <Input
-          onChange={() => {}}
+          onChange={(e) => onChange(e.target.value)}
           value={value}
-          disabled={false}
+          disabled={isDisabled}
           placeholder="Send a message"
-          className={cn("border-white/10")}
+          className={cn("border-white/10", isFollowersOnly && "rounded-t-none border-t-0")}
         />
       </div>
       <div className="ml-auto">
-        <Button type="submit" size="sm" disabled={false}>
+        <Button type="submit" size="sm" disabled={isDisabled}>
           Send
         </Button>
       </div>
     </form>
+  );
+};
+
+export const ChatFormSkeleton = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4 p-3">
+      <Skeleton className="w-full h-10" />
+      <div className="flex items-center gap-x-2 ml-auto">
+        <Skeleton className="h-7 w-7" />
+        <Skeleton className="h-7 w-12" />
+      </div>
+    </div>
   );
 };

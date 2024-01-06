66fc1cfd86e88e8cc0538f319eb905426b77f5a4
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-06 22:37:45 +0530

    add: room service

--- actions/block.ts
+++ actions/block.ts
@@ -1,22 +1,36 @@
 "use server";
 
+import { getUser } from "@/lib/auth-service";
 import { blockUser, unBlockUser } from "@/lib/block-service";
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
+
 export const onBlock = async (id: string) => {
+  const currentUser = await getUser();
+
+  let blockedUser;
+
   try {
-    const blockedUser = await blockUser(id);
-
-    revalidatePath("/");
-
-    if (blockedUser) {
-      revalidatePath(`/${blockedUser.blocked.username}`);
-    }
-
-    return blockedUser;
-  } catch (error) {
-    throw new Error("Internal Error");
+    blockedUser = await blockUser(id);
+  } catch {
+    //This means user is a guest
   }
+
+  try {
+    await roomService.removeParticipant(currentUser.id, id);
+  } catch {
+    //This means user is not in the room
+  }
+
+  revalidatePath(`/u/${currentUser.username}/community`);
+
+  return blockedUser;
 };
 
 export const onUnBlock = async (id: string) => {

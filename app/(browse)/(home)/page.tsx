11d5bccf37603267a1a147e7407e4111988dbc8d
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-07 14:17:37 +0530

    add: home feed

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import { Suspense } from "react";
+import { FeedResults, FeedResultsSkeleton } from "./_components/feed-results";
 
 export default function Home() {
   return (
-    <div className="">
-      <UserButton afterSignOutUrl="/" />
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<FeedResultsSkeleton />}>
+        <FeedResults />
+      </Suspense>
     </div>
   );
 }

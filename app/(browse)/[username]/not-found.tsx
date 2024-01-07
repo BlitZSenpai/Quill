8d6f918f3836dc7d6951431eb1b22e16acb53b7c
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-07 07:24:45 +0530

    add: not found page

---
+++ app/(browse)/[username]/not-found.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col text-muted-foreground space-y-4 h-full items-center justify-center">
+      <h1 className="text-4xl">404</h1>
+      <p>We couldn&apos;t find the user you were looking for.</p>
+      <Button variant="secondary" asChild>
+        <Link href="/">Go back home</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default NotFoundPage;

446d4cd5bc1a64ace6c9806322f7113e3fbc1724
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-07 13:37:16 +0530

    add: get streams algo

---
+++ lib/feed-service.ts
@@ -0,0 +1,58 @@
+import { getUser } from "./auth-service";
+import { db } from "./db";
+
+export const getStreams = async () => {
+  let userId;
+
+  try {
+    const user = await getUser();
+    userId = user.id;
+  } catch {
+    userId = null;
+  }
+
+  let streams = [];
+
+  if (userId) {
+    streams = await db.stream.findMany({
+      where: {
+        user: {
+          NOT: {
+            blocking: {
+              some: {
+                blockedId: userId,
+              },
+            },
+          },
+        },
+      },
+      include: {
+        user: true,
+      },
+      orderBy: [
+        {
+          isLive: "desc",
+        },
+        {
+          updatedAt: "desc",
+        },
+      ],
+    });
+  } else {
+    streams = await db.stream.findMany({
+      include: {
+        user: true,
+      },
+      orderBy: [
+        {
+          isLive: "desc",
+        },
+        {
+          updatedAt: "desc",
+        },
+      ],
+    });
+  }
+
+  return streams;
+};
